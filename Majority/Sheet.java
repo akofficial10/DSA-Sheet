afb75e067e47dbdce5aaf33273dbf5b3f86c1f84
Ayush Kumar <akofficial1330@gmail.com>
2025-09-26 22:50:03 +0530

    Latest

--- Majority/Sheet.java
+++ Majority/Sheet.java
@@ -1,19 +1,27 @@
-package Majority; 
+package Majority;
 
 class Sheet {
     public int majorityElement(int[] nums){
+        // Initialize frequency counter and candidate for majority element
         int freq = 0, ans = 0;
+
+        // Iterate through the array
         for(int i = 0; i < nums.length; i++){
+            // If frequency is 0, assign current element as candidate
             if(freq == 0){
                 ans = nums[i];
             }
+
+            // If current element matches candidate, increase frequency
             if(ans == nums[i]){
                 freq++;
             } else {
+                // Otherwise, decrease frequency
                 freq--;
             }
         }
 
+        // Return the candidate (guaranteed majority element by problem constraints)
         return ans;
     }
 }
--- Merge 2 sorted array/Sheet.java
+++ Merge 2 sorted array/Sheet.java
@@ -1,16 +1,22 @@
 class Sheet {
     public void merge(int[] A, int m, int[] B, int n) {
-        int idx = m+n-1, i = m-1, j = n-1;
+        // Start filling A from the last index (combined length - 1)
+        int idx = m + n - 1; 
+        // Pointers for last valid element of A and last element of B
+        int i = m - 1, j = n - 1;
         
-        while ( i >= 0 && j >= 0){
-            if(A[i] >= B[j]){
+        // Merge from the back while both arrays have elements
+        while (i >= 0 && j >= 0) {
+            if (A[i] >= B[j]) {
+                // Place the larger element at the current index
                 A[idx--] = A[i--];
             } else {
                 A[idx--] = B[j--];
             }
         }
 
-        while ( j >= 0){
+        // If B still has remaining elements, copy them into A
+        while (j >= 0) {
             A[idx--] = B[j--];
         }
     }
